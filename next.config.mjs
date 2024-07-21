43efb990ed451515d6471cc0684ac1a561a618d5
jzy42459 <jzy42459@ly.com>
2024-07-21 18:36:42 +0800

    ci: add code-inspector-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+/** Code Inspector: https://inspector.fe-dev.cn */
+import { codeInspectorPlugin } from "code-inspector-plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(
+      codeInspectorPlugin({ bundler: "webpack" })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
